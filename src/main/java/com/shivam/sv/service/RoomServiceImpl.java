4c82ca8926dc0f87cbd9b32a8825df8c16a33ef8
Shivam verma <shivamverma@Shivams-MacBook-Air-3.local>
2024-03-29 14:40:56 +0530

    add

--- src/main/java/com/shivam/sv/service/RoomServiceImpl.java
+++ src/main/java/com/shivam/sv/service/RoomServiceImpl.java
@@ -1,5 +1,6 @@
 package com.shivam.sv.service;
 
+import com.shivam.sv.exception.ResourceNotFoundException;
 import com.shivam.sv.model.Room;
 import com.shivam.sv.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -37,5 +39,23 @@ public class RoomServiceImpl implements RoomService{
     @Override
     public List<String> getAllRoomTypes() {
         return roomRepository.finDistinctRoomTypes();
+    }
+
+    @Override
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public byte[] getRoomPhotoById(Long roomId) throws SQLException {
+        Optional<Room> theRoom=roomRepository.findById(roomId);
+        if (theRoom.isEmpty()){
+            throw new ResourceNotFoundException("Sorry, room not found");
+        }
+        Blob photoBlob=theRoom.get().getPhoto();
+        if(photoBlob!=null){
+            return photoBlob.getBytes(1,(int)photoBlob.length());
+        }
+        return null;
     }
 }
